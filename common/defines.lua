2483de55589d3af9e67313105922146f55097948
Aluazard <hixameseamusfd@gmail.com>
2019-09-30 09:54:30 -0500

    Added some more provinces

--- common/defines.lua
+++ common/defines.lua
@@ -1509,7 +1509,7 @@ NMilitary = {
 },
 
 NTechnology = {
-	DONT_EXECUTE_TECH_BEFORE = 200,
+	DONT_EXECUTE_TECH_BEFORE = 10000,
 	POINTS_PER_ATTRIBUTE = 0.04,
 
 	BASE_NEIGHBOUR_SPREAD_BONUS = 0.03, 		-- bonus for each neighbour with the tech
@@ -1519,8 +1519,8 @@ NTechnology = {
 	TRADEPOST_SPREAD_BONUS = 0.002, 			-- Spread from trade posts
 	SPYACTION_SPREAD_BONUS = 0.006,				-- Spread from the target of the study technology spymaster action
 
-	IDEAL_YEAR_LEVEL_0 = 700, 					-- the chance to get a progress to this level will increase after this date and decrease before it
-	IDEAL_YEAR_LEVEL_8 = 1500,
+	IDEAL_YEAR_LEVEL_0 = 14000, 					-- the chance to get a progress to this level will increase after this date and decrease before it
+	IDEAL_YEAR_LEVEL_8 = 16400,
 	IDEAL_YEAR_AHEAD_PENALTY_INVEST = 0.6,		-- Percent increase in cost pre level ahead of ideal date
 
 	PAGAN_HOME_ATTRITION_REMOVAL_LEVEL = 4.0,
